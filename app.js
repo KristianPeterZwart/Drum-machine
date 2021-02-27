70827af007e0e46cf24a4a30aacb0f7ca2df84da
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-27 17:19:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 class Drumkit{
     constructor() {
         this.pads = document.querySelectorAll(".pad");
+        this.playBtn = document.querySelector(".play")
         this.kickAudio = document.querySelector(".kick-sound");
         this.snareAudio = document.querySelector(".snare-sound");
         this.hihatAudio = document.querySelector(".hihat-sound");
         this.index = 0;
+        this.bpm = 150;
+    }
+    activePad() {
+        this.classList.toggle("active");
     }
     repeat() {
         let step = this.index % 8;
-        console.log(`step ${step} and index ${this.index}`);
+        const activeBars = document.querySelectorAll(`.b${step}`)
         this.index++;
     }
     start() {
+        const interval = (60/this.bpm) * 1000;
         setInterval(() => {
             this.repeat();
         }, 1000) ;    
@@ -20,4 +26,11 @@ class Drumkit{
 
     const drumkit = new Drumkit();
 
-    drumKit.start();
+    drumkit.pads.forEach(pad => {
+        padd.addEventListener("click", drumkit.activePad);
+    });
+
+
+drumkit.playBtn.addEventListener("click", function(){
+    drumkit.start();
+});   
