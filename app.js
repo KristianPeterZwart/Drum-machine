3f5f6c280a7e7d93a6907124cea79e9fd8acde9a
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-27 10:50:41 +0100

    repeat steps

---
+++ app.js
@@ -0,0 +1,22 @@
+class Drumkit{
+    constructor(){
+        this.pads = document.querySelectorAll(".pad");
+        this.kickAudio = document.querySelector(".kick-sound");
+        this.snareAudio = document.querySelector(".snare-sound");
+        this.hihatAudio = document.querySelector(".hihat-sound");
+        this.index = 0;
+    }
+    repeat() {
+        let step = this.index % 8;
+        console.log(step);
+    }
+    start() {
+        setInterval(() => {
+            this.repeat();
+        }, 1000) ;    
+        }
+    }
+
+    const drumkit = new Drumkit();
+
+    drunkKit.start();
