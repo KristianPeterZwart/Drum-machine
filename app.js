4858c5a46aa4d21ca612fc61fba31a044b096c12
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-27 17:37:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,17 @@ class Drumkit {
         // Loop over the pads
         activeBars.forEach(bar => {
             bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            // Check if pads are active
+            if(bar.classList.contains("active")){
+            // Check each sound
+            if(bar.classList.contains("kick-pad")){
+                this.kickAudio.play();
+            }
+            if(bar.classList.contains("snare-pad")){
+                this.snareAudio.play();
+            }
+            if(bar.classList.contains("hihat-pad")){
+                this.hihatAudio.play();
         });
         this.index++;
     }
